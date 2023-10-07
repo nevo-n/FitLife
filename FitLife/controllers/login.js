186301dc39c7adb5ba540391348f90508daee924
Ori Hadad <orih90@gmail.com>
2023-10-07 19:47:32 +0300

    remove mocks again

--- FitLife/controllers/login.js
+++ FitLife/controllers/login.js
@@ -61,83 +61,6 @@ async function register(req, res) {
  
 }
 
-async function mock1(req, res) {
-  // Step 1: Create Users
-  
-  const mockUsers = [];
-  for (let i = 0; i < 10; i++) {
-      const user = new User({
-          fname: `User${i}`,
-          lname: `LastName${i}`,
-          email: `user${i}@example.com`,
-          password: 'password123',
-          date_of_birth: new Date(),
-          type: 'trainee',
-          status: 'Active'
-      });
-      await user.save();
-      mockUsers.push(user);
-  }
-
-  // Step 2: Create Groups
-  const groupOwners = mockUsers.slice(0, 5);
-  for (const owner of groupOwners) {
-      const group = new Group({
-          creator: owner._id,
-          name: `GroupOf${owner.fname}`,
-          description: `This is a group owned by ${owner.fname}`,
-          tags: ['tag1', 'tag2', 'tag3'],
-          freinds: [owner._id]
-      });
-      
-      // Step 3: Add Friends to Groups
-      const randomFriends = mockUsers.filter(u => u._id.toString() !== owner._id.toString());
-      group.friends = randomFriends.slice(0, Math.floor(Math.random() * 3) + 2).map(u => u._id);
-      
-      await group.save();
-      owner.groups.push(group._id);
-      await owner.save();
-      
-      // Step 4: Create Posts for Groups
-      for (let i = 0; i < 7; i++) {
-        const randomFriendId = group.friends[Math.floor(Math.random() * group.friends.length)];
-
-        // Find postAuthor
-        const postAuthor = mockUsers.find(u => u._id.equals(randomFriendId));
-
-        // Check if postAuthor is found
-        if (!postAuthor) {
-            console.error(`No user found for ID ${randomFriendId}. Skipping post creation.`);
-            continue;
-        }
-
-        // If postAuthor is found, create a new post
-        const post = new Post({
-            author: postAuthor._id,
-            content: `This is a post ${i} in ${group.name}`,
-            title: `Post ${i} Title`
-        });
-        await post.save();
-        
-        // Add this post to the postAuthor's posts field
-        postAuthor.posts.push(post._id);
-        await postAuthor.save();
-
-        group.posts.push(post._id);
-        await group.save();
-
-      }
-  }
-
-  // Step 5: Update User Following
-  for (const user of mockUsers) {
-      const randomUsers = mockUsers.filter(u => u._id.toString() !== user._id.toString());
-      user.following = randomUsers.slice(0, 5).map(u => u._id);
-      await user.save();
-  }
-
-  res.send("done")
-}
 
 
 
@@ -148,5 +71,5 @@ module.exports = {
   registerForm,
   logout,
   isLoggedIn,
-  mock1
+
 }
--- FitLife/routes/login.js
+++ FitLife/routes/login.js
@@ -11,7 +11,4 @@ router.post("/login", loginController.login);
 
 router.get('/logout',loginController.logout);
 
-router.get('/mock1', loginController.mock1)
-//router.get('/', loginController.isLoggedIn, loginController.foo);
-
 module.exports = router;
